ce74e511e44559a4fb92fbfe0f7b18578b08e0b9
Viswa264506 <viswavarna651@gmail.com>
2026-07-03 15:21:21 +0530

    Add GlobalExceptionHandler.java

---
+++ SkillMarket-Backend/src/main/java/com/skillmarket/skill_marketplace/config/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.skillmarket.skill_marketplace.config;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<Map<String, String>> handleRuntimeException(RuntimeException ex) {
+        return ResponseEntity
+                .badRequest()
+                .body(Map.of("message", ex.getMessage()));
+    }
+}
